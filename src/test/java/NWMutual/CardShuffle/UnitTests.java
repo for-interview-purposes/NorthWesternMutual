23f85ff92a81934d7229ccaa8984825d2f2858d6
Sadat_Riskiyeva <81530644+for-interview-purposes@users.noreply.github.com>
2021-03-28 19:35:58 -0400

    1. class for unit tests was created 2. @BeforeAll and @AfterAll were added

---
+++ src/test/java/NWMutual/CardShuffle/UnitTests.java
@@ -0,0 +1,20 @@
+package NWMutual.CardShuffle;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class UnitTests {
+
+    @BeforeAll
+    public static void before_all_tests_message(){
+        System.out.println("This is Unit Test for NorthWestern Mutual interview assignment");
+    }
+
+
+
+    @AfterAll
+    public static void after_all_tests_message(){
+        System.out.println("This is End of Unit Test for NorthWestern Mutual interview assignment");
+    }
+
+}
