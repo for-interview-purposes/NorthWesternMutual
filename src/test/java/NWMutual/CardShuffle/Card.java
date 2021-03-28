73fe0d5c0a32f1864e027e0ddd9b5d54fee22c9b
Sadat_Riskiyeva <81530644+for-interview-purposes@users.noreply.github.com>
2021-03-28 16:01:57 -0400

    1. declared rank and suit of a Card as enum type 2. generates a constructor that will create objects of Card class with parameters of enum type

---
+++ src/test/java/NWMutual/CardShuffle/Card.java
@@ -0,0 +1,18 @@
+package NWMutual.CardShuffle;
+
+public class Card {
+
+    static final int totalNumOfCardsInADeck = 52;
+
+    public enum RankOfACard{ Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King}
+    public enum SuitOfACard {Clubs, Diamonds, Hearts, Spades}
+
+    private final RankOfACard rankOfACard;
+    private final SuitOfACard suitOfACard;
+
+    private Card(RankOfACard rankOfACard, SuitOfACard suitOfACard){
+        this.rankOfACard = rankOfACard;
+        this.suitOfACard = suitOfACard;
+    }
+
+}
