d6cf13ab2dd63343924fc850743551bd828fe270
Sadat_Riskiyeva <81530644+for-interview-purposes@users.noreply.github.com>
2021-03-28 20:30:07 -0400

    tests added: 1. check number of card objects in initial deck/array equals 52 2. deck/array of Card objects to be used for game wquals initial deck/array of Card objects

--- src/test/java/NWMutual/CardShuffle/UnitTests.java
+++ src/test/java/NWMutual/CardShuffle/UnitTests.java
@@ -1,7 +1,11 @@
 package NWMutual.CardShuffle;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 public class UnitTests {
 
@@ -10,6 +14,22 @@ public class UnitTests {
         System.out.println("This is Unit Test for NorthWestern Mutual interview assignment");
     }
 
+    @Test
+    public void check_number_of_cards_in_an_initial_deck(){
+        int numberOfCardsInDeck = 52;
+        int actual = Card.InitialDeck.size();
+
+        Assert.assertEquals(numberOfCardsInDeck, actual);
+    }
+
+    @Test
+    public void new_deck_equals_initial_deck(){
+        ArrayList<Card> newDeck = Card.newDeck();
+        ArrayList<Card> initialDeck = (ArrayList<Card>) Card.InitialDeck;
+
+        Assert.assertEquals(newDeck, initialDeck);
+
+    }
 
 
     @AfterAll
