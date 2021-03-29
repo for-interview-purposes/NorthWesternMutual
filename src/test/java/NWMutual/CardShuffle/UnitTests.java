9411abe2a5c13dc0d8f091889ffe407142e21514
Sadat_Riskiyeva <81530644+for-interview-purposes@users.noreply.github.com>
2021-03-28 20:42:22 -0400

    test created calculating correct number of cards to players

--- src/test/java/NWMutual/CardShuffle/UnitTests.java
+++ src/test/java/NWMutual/CardShuffle/UnitTests.java
@@ -31,6 +31,37 @@ public class UnitTests {
 
     }
 
+    @Test
+    public void dealing_correct_number_of_cards_to_players(){
+        int numberOfPlayers2 = 2;
+        int numberOfPlayers3 = 3;
+        int numberOfPlayers4 = 4;
+        int numberOfPlayers5 = 5;
+        int numberOfPlayers6 = 6;
+        int numberOfPlayers7 = 7;
+
+        int expectedResult2 = 26;
+        int expectedResult3 = 17;
+        int expectedResult4 = 13;
+        int expectedResult5 = 10;
+        int expectedResult6 = 8;
+        int expectedResult7 = 7;
+
+        int actualResult2 = Card.dealingTheDeck(Card.newDeck(), numberOfPlayers2).size();
+        int actualResult3 = Card.dealingTheDeck(Card.newDeck(), numberOfPlayers3).size();
+        int actualResult4 = Card.dealingTheDeck(Card.newDeck(), numberOfPlayers4).size();
+        int actualResult5 = Card.dealingTheDeck(Card.newDeck(), numberOfPlayers5).size();
+        int actualResult6 = Card.dealingTheDeck(Card.newDeck(), numberOfPlayers6).size();
+        int actualResult7 = Card.dealingTheDeck(Card.newDeck(), numberOfPlayers7).size();
+
+        Assert.assertEquals(expectedResult2, actualResult2);
+        Assert.assertEquals(expectedResult3, actualResult3);
+        Assert.assertEquals(expectedResult4, actualResult4);
+        Assert.assertEquals(expectedResult5, actualResult5);
+        Assert.assertEquals(expectedResult6, actualResult6);
+        Assert.assertEquals(expectedResult7, actualResult7);
+
+    }
 
     @AfterAll
     public static void after_all_tests_message(){
